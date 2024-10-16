db7e50a3116de3b79fc6d709a24af3f3d6875f9f
xfurqan0 <yldzfurkann0@gmail.com>
2024-10-16 20:49:37 +0300

    Odev2
    klavyeden girilen N pozitif tam sayisindan 0'a kadar olan tam sayilari 
    listeleyen programi "while" döngüsü kullanarak yazın

---
+++ Programalama/week3/problemset/odev2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N,i;
+    printf(" bir  sayi giriniz");
+    scanf("%d",&N);
+    i=N;
+    while(i>0)
+    {
+        printf("%4d\n",i);
+    i--;
+    }
+
+
+    return 0;
+}
