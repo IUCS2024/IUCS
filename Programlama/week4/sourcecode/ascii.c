df6ae1b655f559c6385ac723c2e14dd03e92f585
xfurqan0 <yldzfurkann0@gmail.com>
2024-10-23 21:48:16 +0300

    girdiğin karakterin ascii değerini yazıyor

---
+++ Programlama/week4/sourcecode/ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    char c;
+    printf("Bir karakter girin: ");
+    scanf("%c",&c);
+    printf("%c\n",c);
+    printf("ascii kodunu :%d\n",(int)c);
+    return 0;
+}
